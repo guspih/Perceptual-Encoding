b4d55f41fddf65a3d5f07b2ac6032a8b5234f3e6
Gustav Grund Pihlgren <gustav.pihlgren@ltu.se>
2019-05-24 15:25:53 +0200

    Restructured npz file loading to be more general

--- networks_test.py
+++ networks_test.py
@@ -82,7 +82,8 @@ def load_npz_data(data_file, data_size, batch_size,
         else:
             value = value[:data_size]
             value = np.split(value, value.shape[0]/batch_size)
-        if isinstance(value[0], np.ndarray):
+        if (isinstance(value[0], np.ndarray) and 
+                value[0].dtype.kind in ["f","u","i"]):
             value = [torch.from_numpy(batch) for batch in value]
             if gpu:
                 value = [batch.cuda() for batch in value]
